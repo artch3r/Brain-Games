d3a8d9e070a14b8d76e50de277a82b5298c003df
Артем Черных <acac1212@yandex.ru>
2022-08-04 17:43:23 +1000

    new version of brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
-import brainEvenGame from '../src/games/brain-even-game.js';
-console.log('hihi');
-brainEvenGame();
+import { getRandomNumber, gameEngine } from '../src/index.js';
+
+const questionText = 'Answer "yes" if number is even, otherwise answer "no"';
+
+const questionsAndAnswers = [
+  [],
+  [],
+  [],
+];
+
+for (let i = 0; i < 3; i += 1) {
+  const question = getRandomNumber(10);
+  const answer = question % 2 === 0 ? 'yes' : 'no';
+  questionsAndAnswers[i][0] = question;
+  questionsAndAnswers[i][1] = answer;
+}
+
+console.log('start');
+gameEngine(questionText, questionsAndAnswers);
