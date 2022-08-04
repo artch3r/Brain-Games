6f020bc4e6565c885d210c7255c060c591bfd89a
Артем Черных <acac1212@yandex.ru>
2022-08-04 12:14:45 +1000

    add src/index.js as common game engine

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import readlineSync from 'readline-sync';
 
-const getRandomNumber = () => Math.round(Math.random() * 100);
+const getRandomNumber = () => Math.round(Math.random() * 10);
 
-const gameEngine = (questionText, questionExpression, checkIsRight) => {
+const gameEngine = (questionText, questionsAndAnswers) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(questionText);
-  let score = 0;
 
-  for (let turn = 1; turn <= 3; turn += 1) {
-    console.log(`Question: ${questionExpression()}`);
+  for (let round = 0; round < questionsAndAnswers.length; round += 1) {
+    const [question, correctAnswer] = questionsAndAnswers[round];
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (checkIsRight()) {
+    if (Number(userAnswer) === correctAnswer) {
       console.log('Correct!');
-      score += 1;
-    } else if (!checkIsRight()) {
+    } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
       Let's try again, ${name}!`);
       break;
     }
-  }
-
-  if (score === 3) {
     console.log(`Congratulations, ${name}!`);
   }
 };
