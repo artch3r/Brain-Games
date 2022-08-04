7bcf73389b1a61395b8448779d1e1cddb84c51f0
Артем Черных <acac1212@yandex.ru>
2022-08-04 12:13:08 +1000

    fix brain-calc.js to actual version

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,27 +4,39 @@ import { getRandomNumber, gameEngine } from '../src/index.js';
 
 const questionText = 'What is the result of the expression?';
 
-const questionExpressionFunction = () => {
-  const helperForExpression = () => {
-    const number1 = getRandomNumber();
-    const number2 = getRandomNumber();
-    const expression = `${number1} + ${number2}`;
-    return expression;
-  };
-  return helperForExpression;
-};
-const questionExpression = questionExpressionFunction();
+const questionsAndAnswers = [
+  [],
+  [],
+  [],
+];
 
-const checkIsRightFunction = () => {
-  const helperForCheckIsRight = () => {
-    const correctAnswer = number1 + number2;
-    if (Number(userAnswer) === correctAnswer) {
-      return true;
-    }
-    return false;
-  };
-  return helperForCheckIsRight;
-};
-const checkIsRight = checkIsRightFunction();
+const mathOperators = ['+', '-', '*'];
+const getRandomMathOperator = (operators) => operators[Math.round(Math.random() * 2)];
 
-gameEngine(questionText, questionExpression, checkIsRight);
+for (let i = 0; i < 3; i += 1) {
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+  const mathOperator = getRandomMathOperator(mathOperators);
+  let question;
+  let answer;
+  switch (mathOperator) {
+    case '+':
+      question = `${number1} + ${number2}`;
+      answer = number1 + number2;
+      break;
+    case '-':
+      question = `${number1} - ${number2}`;
+      answer = number1 - number2;
+      break;
+    case '*':
+      question = `${number1} * ${number2}`;
+      answer = number1 * number2;
+      break;
+    default:
+      break;
+  }
+  questionsAndAnswers[i][0] = question;
+  questionsAndAnswers[i][1] = answer;
+}
+
+gameEngine(questionText, questionsAndAnswers);
