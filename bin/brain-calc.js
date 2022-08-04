78252cb27465b97914e9fff4fa50a12580dab98b
Артем Черных <acac1212@yandex.ru>
2022-08-04 14:17:02 +1000

    add games directory and fix brain-calc to use it

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,42 +1,5 @@
 #!/usr/bin/env node
 
-import { getRandomNumber, gameEngine } from '../src/index.js';
+import brainCalcGame from '../src/games/brain-calc-game.js';
 
-const questionText = 'What is the result of the expression?';
-
-const questionsAndAnswers = [
-  [],
-  [],
-  [],
-];
-
-const mathOperators = ['+', '-', '*'];
-const getRandomMathOperator = (operators) => operators[getRandomNumber(2)];
-
-for (let i = 0; i < 3; i += 1) {
-  const number1 = getRandomNumber(10);
-  const number2 = getRandomNumber(10);
-  const mathOperator = getRandomMathOperator(mathOperators);
-  let question;
-  let answer;
-  switch (mathOperator) {
-    case '+':
-      question = `${number1} + ${number2}`;
-      answer = number1 + number2;
-      break;
-    case '-':
-      question = `${number1} - ${number2}`;
-      answer = number1 - number2;
-      break;
-    case '*':
-      question = `${number1} * ${number2}`;
-      answer = number1 * number2;
-      break;
-    default:
-      break;
-  }
-  questionsAndAnswers[i][0] = question;
-  questionsAndAnswers[i][1] = answer;
-}
-
-gameEngine(questionText, questionsAndAnswers);
+brainCalcGame();
