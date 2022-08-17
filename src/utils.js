dfeb9b0ca61bde3fae34c6d9133533fcbd5fedc8
Артем Черных <acac1212@yandex.ru>
2022-08-17 10:37:08 +1000

    fix getRandomIndex

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,5 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const getRandomIndex = (array) => {
-  const firstIndex = 0;
-  const lastIndex = array.length - 1;
-  const randomIndex = getRandomNumber(firstIndex, lastIndex);
-  return randomIndex;
-};
+const getRandomIndex = (data) => getRandomNumber(0, data.length - 1);
 
 export { getRandomNumber, getRandomIndex };
